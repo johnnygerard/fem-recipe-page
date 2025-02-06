00da22acc3c371b7c4cbace379d61f8c7deba273
Johnny Gérard <john@jgerard.dev>
2025-02-06 16:38:02 +0100

    feat: add h2.tsx

---
+++ src/components/h2.tsx
@@ -0,0 +1,15 @@
+import { memo } from "react";
+
+type Props = Readonly<{
+  children: string;
+}>;
+
+const H2 = ({ children }: Props) => {
+  return (
+    <h1 className="text-brown-800 font-serif text-[1.75rem] leading-none">
+      {children}
+    </h1>
+  );
+};
+
+export default memo(H2);
