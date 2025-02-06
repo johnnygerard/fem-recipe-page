6f7067c6a7cf909bc2bc0bbccb6d1189274a7eaa
Johnny Gérard <john@jgerard.dev>
2025-02-06 19:42:59 +0100

    feat: add omelet image

--- src/components/recipe.tsx
+++ src/components/recipe.tsx
@@ -1,5 +1,7 @@
+import omelet from "@/assets/omelet.jpg";
 import H1 from "@/components/h1";
 import { clsx } from "clsx";
+import Image from "next/image";
 import { memo } from "react";
 
 const Recipe = () => {
@@ -7,9 +9,20 @@ const Recipe = () => {
     <div
       className={clsx(
         "tb:rounded-300 bg-white",
-        "tb:w-[38.5rem] dt:w-[46rem] tb:p-500 px-400 pb-500",
+        "tb:w-[38.5rem] dt:w-[46rem] tb:p-500 pb-500",
       )}
     >
+      <div className="tb:h-[18.75rem] relative h-[10.6875rem] w-full">
+        <Image
+          className="tb:rounded-150 object-cover"
+          src={omelet}
+          alt="A white plate topped with an omelet and greens"
+          sizes="(min-width: 64em) 41rem, (min-width: 48em) 33.5rem, 100vw"
+          fill
+          placeholder="blur"
+          priority
+        />
+      </div>
       <H1>Simple Omelette Recipe</H1>
     </div>
   );
