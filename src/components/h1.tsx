a6a3c1b5e97cf825ba53327823073ba7ec4c1fd1
Johnny Gérard <john@jgerard.dev>
2025-02-06 16:31:41 +0100

    feat: add h1.tsx

---
+++ src/components/h1.tsx
@@ -0,0 +1,15 @@
+import { memo, ReactNode } from "react";
+
+type Props = Readonly<{
+  children: ReactNode;
+}>;
+
+const H1 = ({ children }: Props) => {
+  return (
+    <h1 className="tb:text-[2.5rem] font-serif text-[2.25rem] leading-none text-stone-900">
+      {children}
+    </h1>
+  );
+};
+
+export default memo(H1);
