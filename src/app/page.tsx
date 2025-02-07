b79524f7ff983d06ddec04e225ea163097f7f5ad
Johnny Gérard <john@jgerard.dev>
2025-02-07 12:45:09 +0100

    feat: set homepage minimum width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 
 const HomePage = () => {
   return (
-    <div className="tb:py-1600 tb:grid tb:place-content-center min-h-screen w-full">
+    <div className="tb:py-1600 tb:grid tb:place-content-center min-h-screen w-full min-w-min">
       <Recipe />
     </div>
   );
