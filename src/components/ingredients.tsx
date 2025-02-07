f76ac220d8f749f5dc36198365ae3e0fb1b4135b
Johnny Gérard <john@jgerard.dev>
2025-02-07 11:47:49 +0100

    refactor: improve ingredients.tsx

--- src/components/ingredients.tsx
+++ src/components/ingredients.tsx
@@ -2,19 +2,21 @@ import H2 from "@/components/h2";
 import UnorderedList from "@/components/unordered-list";
 import { Fragment, memo } from "react";
 
+const ingredients = [
+  "2-3 large eggs",
+  "Salt, to taste",
+  "Pepper, to taste",
+  "1 tablespoon of butter or oil",
+  "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
+];
+
 const Ingredients = () => {
   return (
     <section>
       <H2>Ingredients</H2>
       <UnorderedList bulletColor="bg-brown-800">
-        {[
-          "2-3 large eggs",
-          "Salt, to taste",
-          "Pepper, to taste",
-          "1 tablespoon of butter or oil",
-          "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
-        ].map((ingredient) => (
-          <Fragment key={ingredient}>{ingredient}</Fragment>
+        {ingredients.map((ingredient, index) => (
+          <Fragment key={index}>{ingredient}</Fragment>
         ))}
       </UnorderedList>
     </section>
